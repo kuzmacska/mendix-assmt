8cf11ddf0e6f48a8f6f2593abcb301ae8fae6c7d
kuzmacska <pnd-kuzm@mail.ru>
2019-09-30 13:32:00 +0300

    refactored values.yaml changed path for jenkins values

--- modules/helm-jenkins/main.tf
+++ modules/helm-jenkins/main.tf
@@ -2,20 +2,11 @@ locals {
   jenkins_namespace = "jenkins"
 }
 
-resource "template_file" "helm-jenkins-values-template" {
-  template = "${file("${path.module}/resources/values-tmpl.yaml")}"
-  vars {
-
-  }
-}
-
 resource "helm_release" "jenkins" {
   chart = "stable/jenkins"
   name = "jenkins"
   version = "${var.jenkins_helm_version}"
   namespace = "${local.jenkins_namespace}"
-  values = [
-    "${template_file.helm-jenkins-values-template.rendered}"
-  ]
+  values = ["${file("${path.module}/resources/values.yaml")}"]
 }
 
