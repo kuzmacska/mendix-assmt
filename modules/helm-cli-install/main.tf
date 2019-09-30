c6961b3d38faee4c4732ce9f568ab926dea938d1
kuzmacska <pnd-kuzm@mail.ru>
2019-09-30 20:14:22 +0300

    terraform formatted

--- modules/helm-cli-install/main.tf
+++ modules/helm-cli-install/main.tf
@@ -1,41 +1,41 @@
 resource "null_resource" "helm-install" {
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "curl -sO https://storage.googleapis.com/kubernetes-helm/helm-${var.helm_version}-linux-amd64.tar.gz"
+    command     = "curl -sO https://storage.googleapis.com/kubernetes-helm/helm-${var.helm_version}-linux-amd64.tar.gz"
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "curl -sO https://storage.googleapis.com/kubernetes-helm/helm-${var.helm_version}-linux-amd64.tar.gz.sha256"
+    command     = "curl -sO https://storage.googleapis.com/kubernetes-helm/helm-${var.helm_version}-linux-amd64.tar.gz.sha256"
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "openssl sha1 -sha256 helm-v2.12.1-linux-amd64.tar.gz | awk '{print $2}' > helm_sha"
+    command     = "openssl sha1 -sha256 helm-v2.12.1-linux-amd64.tar.gz | awk '{print $2}' > helm_sha"
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "if [ $(cat helm_sha) != $(cat helm-${var.helm_version}-linux-amd64.tar.gz.sha256) ]; then echo 'helm sha is incorrect'; fi"
+    command     = "if [ $(cat helm_sha) != $(cat helm-${var.helm_version}-linux-amd64.tar.gz.sha256) ]; then echo 'helm sha is incorrect'; fi"
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "tar -xvzf helm-${var.helm_version}-linux-amd64.tar.gz --strip 1"
+    command     = "tar -xvzf helm-${var.helm_version}-linux-amd64.tar.gz --strip 1"
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "chmod +x helm"
+    command     = "chmod +x helm"
   }
 
   provisioner "local-exec" {
     working_dir = "${path.module}/resources"
-    command = "mv helm /usr/local/bin"
+    command     = "mv helm /usr/local/bin"
   }
 
   provisioner "local-exec" {
     working_dir = "/usr/local/bin"
-    command = "helm init --client-only"
+    command     = "helm init --client-only"
   }
 }
--- modules/helm-jenkins/main.tf
+++ modules/helm-jenkins/main.tf
@@ -3,10 +3,9 @@ locals {
 }
 
 resource "helm_release" "jenkins" {
-  chart = "stable/jenkins"
-  name = "jenkins"
-  version = "${var.jenkins_helm_version}"
+  chart     = "stable/jenkins"
+  name      = "jenkins"
+  version   = "${var.jenkins_helm_version}"
   namespace = "${local.jenkins_namespace}"
-  values = ["${file("${path.module}/resources/values.yaml")}"]
+  values    = ["${file("${path.module}/resources/values.yaml")}"]
 }
-
